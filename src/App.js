e29fbaa2af8cbf95a35ef685c5bf0a5e5c5362ea
Dhaesha M <dhaesha1@gmail.com>
2022-04-10 16:09:05 -0400

    rendering 3d scene w no errors

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import "./index.css";
+import React, { useRef, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, useGLTF } from "@react-three/drei";
+
 function App() {
-  return <div className="App">App</div>;
+  const ref = useRef();
+  return (
+    <div className="App">
+      <Canvas>
+        <mesh ref={ref}>
+          <boxGeometry attach="geometry" args={[1, 1, 1]} />
+        </mesh>
+      </Canvas>
+    </div>
+  );
 }
 
 export default App;
